cb6b4056c2b09443653f609a2fd8b53c8a565d4c
ShivaEshwaraPrasad <110898453+ShivaEshwaraPrasad@users.noreply.github.com>
2022-08-18 12:59:17 +0530

    Add files via upload

---
+++ Five.java
@@ -0,0 +1,12 @@
+class Five
+  {
+    public static void main(String args[])
+    {
+      String st="karun";
+      String st1=new String("Hello");
+      
+      System.out.println("The Given name is:"+st);
+      System.out.println("The Given name is:"+st1);
+      
+    }
+  }
