c17ca6ed3c7441e80a8080bf9b97cff3c96c0448
ShivaEshwaraPrasad <110898453+ShivaEshwaraPrasad@users.noreply.github.com>
2022-08-18 13:00:40 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,14 @@
+class Employee
+  {
+    public static void main(String args [])
+    {
+      int eno= 101;
+      String ename ="SHIVA";
+      double esal = 454.45;
+      System.out.println("The Given Employee Number is:"+eno);
+      System.out.println("The Given Employee Name is:"+ename);
+      System.out.println("The Given Employee Salary is:"+esal);
+    }
+  }
+      
+      
