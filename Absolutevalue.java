6f65741c84d4a381adff813d1a0151612ce9407e
ShivaEshwaraPrasad <110898453+ShivaEshwaraPrasad@users.noreply.github.com>
2022-08-23 16:17:04 +0530

    Add files via upload

---
+++ Absolutevalue.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Absolutevalue
+  {
+    public static void main (String args[])
+    {
+      int a,b;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a value:");
+      a=sc.nextInt();
+      b=(a*(-1));
+      System.out.println("print a value:"+a); 
+      System.out.println("print absolute integer valueis:"+b); 
+    }
+  }
+      
+     
+      
